f3c8cb15cb16976be545a091c44f20dfd570d6d2
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-29 18:04:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+    end
+    
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    roster = {}
+    
+    @roster.map do |grade, names|
+      puts "Grade: #{names}"
+    end
+  end
+end
